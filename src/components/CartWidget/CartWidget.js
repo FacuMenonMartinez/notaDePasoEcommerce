0436027fc680521db5e7696aa335b44750212da7
Facundo Menon Martinez <facundomenon.m@gmail.com>
2023-07-24 20:29:40 -0300

    agregue bulma al proyecto

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -2,7 +2,7 @@ import carrito from "../../assets/carrito.png";
 
 function CartWidget (){
     return (
-        <div className="div-carrito">
+        <div className="div-carrito column navbar-item  is-one-quarter">
             <img className="carrito-logo"  src={carrito} alt="Carrito de Compras" />
             <div className="contador-carrito">0</div>
         </div>
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,16 +3,29 @@ import icono from "../../assets/icono.png";
 
 function NavBar (){
     return (
-        <nav className="navBar">
-            <div>
+
+        
+        
+        <nav className="navbar navBar is-black columns">
+            <div className="column is-one-quarter">
                 <img className="logo" src={icono} alt="Logo del sitio"></img>
             </div>
-            <div>
-                <button>Instrumentos</button>
-                <button>Partituras</button>
-                <button>Contacto</button>
+            <div className="column is-half is-flex is-justify-content-space-between">
+            <a className="has-text-white">Instrumentos</a>
+            <a className="has-text-white">Partituras</a>
+            <a className="has-text-white">Contactos</a>
+            {/* <div className="column has-background-danger navbar-item"></div>
+            <div className="column has-background-link navbar-item"></div>
+            <div className="column has-background-primary navbar-item"></div> */}
             </div>
-            <CartWidget/>
+               
+                <CartWidget/>
+
+            <a className="navbar-burger">
+                <span></span>
+                <span></span>
+                <span></span>
+            </a>
         </nav>
 
     )
