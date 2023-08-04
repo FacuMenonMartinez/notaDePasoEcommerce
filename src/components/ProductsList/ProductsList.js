2b59e28fa65744d0161a3b4e8506a1d2dda141f7
Facundo Menon Martinez <facundomenon.m@gmail.com>
2023-08-04 10:49:33 -0300

    principio de axios

---
+++ src/components/ProductsList/ProductsList.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+
+  function ProductsList (){
+    
+
+    return(
+        <div>
+            <h4>Lista de productos</h4>
+        </div>
+    )
+}
+
+export default ProductsList
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,14 +1,18 @@
 import ItemListContainer from "../../components/ItemListContainer/ItemListContainer";
 import ElementoBanner from "../../components/ElementoBannerHome/HomeBanner";
+import ProductsList from "../../components/ProductsList/ProductsList";
 import "./Home.css"
 
 function Home(){
     return(
         <div>
-            <ItemListContainer greetin='Bienvenidos a mi tienda Online'/>
+            <ItemListContainer greetin='Bienvenidos a mi tienda online'/>
             <div className="contenedorBanner">
                 <ElementoBanner claseBanner="bannerIndividual" claseContenedor="elementoBanner elementoBannerInstrumentos" claseContenedorTexto="contenedorTexto" claseTexto="textoBanner" redireccion="/instrumentos" nombreElemento='Instrumentos'/>
                 <ElementoBanner claseBanner="bannerIndividual" claseContenedor="elementoBanner elementoBannerPartituras" claseContenedorTexto="contenedorTexto" claseTexto="textoBanner" redireccion="/partituras" nombreElemento='Partituras'/>
+            </div>
+            <div>
+                <ProductsList/>
             </div>
         </div>
     )
